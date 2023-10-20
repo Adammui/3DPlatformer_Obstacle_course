231921caf578e1ba12475b99380e75e79204b578
Adammui <Adami.yuje@gmail.com>
2023-10-20 09:54:48 +0300

    change directions on wind platform

--- Assets/Scripts/PlatformWindController.cs
+++ Assets/Scripts/PlatformWindController.cs
@@ -1,30 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlatformWindController : MonoBehaviour
 {
     [SerializeField]
     List<CharacterController> CharactersInWindZoneList = new List<CharacterController>();
+
     [SerializeField]
-    private Vector3 windDirection = Vector3.right;
+    private Vector3 _windDirection;
+
+    private Vector3[] _arrayOfVectors = new[] { new Vector3(0.0f, 0.0f, -1.0f), new Vector3(0.0f, 0.0f, 1.0f), 
+                                                new Vector3(1.0f, 0.0f, 0.0f), new Vector3(-1.0f, 0.0f, -1.0f) };
+
     [SerializeField]
-    private float windStrength = 5;
+    private float _windStrength = 5;
+
+    [SerializeField]
+    private float _changeWindTime = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeWindDirection();
+    }
 
     // Is called when any collider enters trigger of platform
-    private void OnTriggerEnter(Collider col)
+    private void OnTriggerEnter(Collider collider)
     {
-        CharacterController _characterController = col.gameObject.GetComponent<CharacterController>();
+        CharacterController _characterController = collider.gameObject.GetComponent<CharacterController>();
         if (_characterController != null)
             CharactersInWindZoneList.Add(_characterController);
     }
 
     // Is called when collider exits platform
-    private void OnTriggerExit(Collider col)
+    private void OnTriggerExit(Collider collider)
     {
-        CharacterController _characterController = col.gameObject.GetComponent<CharacterController>();
+        CharacterController _characterController = collider.gameObject.GetComponent<CharacterController>();
         if (_characterController != null)
             CharactersInWindZoneList.Remove(_characterController);
+    }
+    private void ChangeWindDirection()
+    {
+        System.Random random = new System.Random();
+        int randomDir = random.Next(0, _arrayOfVectors.Length);
+        _windDirection = _arrayOfVectors[randomDir];
+        StartCoroutine(ChangeWindCoroutine());
     }
 
     // Is called every fixed frame rate.
@@ -34,8 +56,15 @@ public class PlatformWindController : MonoBehaviour
         {
             foreach (CharacterController u in CharactersInWindZoneList)
             {
-                u.Move(windDirection * windStrength * Time.deltaTime);
+                u.Move(_windDirection * _windStrength * Time.deltaTime);
             }
         }
+    }
+    // Provides cooldown for wind direction change
+    private IEnumerator ChangeWindCoroutine()
+    {
+        yield return new WaitForSeconds(_changeWindTime);
+        Debug.Log("changing wind");
+        ChangeWindDirection();
     }
 }
